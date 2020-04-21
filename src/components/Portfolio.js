0d7d85f79c20966e0a55bd2195a92808c83b3d03
Brianna Bell <bbell9563@gmail.com>
2020-04-20 23:29:44 -0600

    fixed error

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import DesktopPortfolio from '../components/container/Portfolio/DesktopPortfolio'
-import MobilePortfolio from '../components/container/Portfolio/MobilePortfolio'
 
 
 const Resume = ({ isMobile }) => (
   <>
-    {isMobile ?
-      <MobilePortfolio />
-      :
-      <DesktopPortfolio />
-    }
+    <div>
+      Portfolio
+    </div>
   </>
 )
 
